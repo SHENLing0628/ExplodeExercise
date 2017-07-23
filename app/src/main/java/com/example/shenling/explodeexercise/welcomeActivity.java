fc357e0a4beca015f63f813c3e66aa332c6435ef
SHENLing0628 <lingshen0628@gmail.com>
2017-07-23 15:05:19 +0800

    TEST

--- app/src/main/java/com/example/shenling/explodeexercise/welcomeActivity.java
+++ app/src/main/java/com/example/shenling/explodeexercise/welcomeActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 public class welcomeActivity extends Activity{
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
